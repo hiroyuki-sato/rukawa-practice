a82114c2162053720a92b7e919390ac13dcce7a0
Hiroyuki Sato <hiroysato@gmail.com>
2016-03-31 22:19:56 +0900

    Add config example

--- rukawa.rb
+++ rukawa.rb
@@ -4,6 +4,15 @@ require 'custom_lib'
 
 Rukawa.configure do |c|
   c.log_file = STDOUT
+#  c.concurrency = 5
+#  c.graph.rankdir     = "LR"
+#  c.graph.size        = TODO
+#  c.graph.rotate      = TODO
+#  c.graph.ranksep     = TODO
+#  c.graph.nodesep     = TODO
+#  c.graph.concentrate = TODO
+#  c.graph.node.shape  = TODO
+#  c.graph.node.style  = TODO
 end
 
 puts Rukawa.config.log_file
